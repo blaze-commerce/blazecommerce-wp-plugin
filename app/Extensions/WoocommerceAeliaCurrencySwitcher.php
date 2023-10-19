40fe0091c30ab133206989eb9c24fc4a9808589f
Alan <alan@blaze.online>
2023-10-19 21:45:01 +0800

    fix: used one function for querying currencies to prevent memory leaks

--- app/Extensions/WoocommerceAeliaCurrencySwitcher.php
+++ app/Extensions/WoocommerceAeliaCurrencySwitcher.php
@@ -20,12 +20,12 @@ class WoocommerceAeliaCurrencySwitcher
     public function __construct()
     {
         if ( is_plugin_active( 'woocommerce-aelia-currencyswitcher/woocommerce-aelia-currencyswitcher.php' ) ) {
-            // add_filter( 'blaze_wooless_product_data_for_typesense', array( $this, 'add_multicurrency_prices' ), 10, 2 );
+            add_filter( 'blaze_wooless_product_data_for_typesense', array( $this, 'add_multicurrency_prices' ), 10, 2 );
             add_filter( 'blaze_wooless_additional_site_info', array( $this, 'add_multicurrency_site_info' ), 10, 1 );
 
-            add_filter('wooless_product_price', array( $this, 'wooless_product_regular_price'), 10, 2);
-            add_filter('wooless_product_regular_price', array( $this, 'wooless_product_regular_price'), 10, 2);
-            add_filter('wooless_product_sale_price', array( $this, 'wooless_product_sale_price'), 10, 2);
+            // add_filter('wooless_product_price', array( $this, 'wooless_product_regular_price'), 10, 2);
+            // add_filter('wooless_product_regular_price', array( $this, 'wooless_product_regular_price'), 10, 2);
+            // add_filter('wooless_product_sale_price', array( $this, 'wooless_product_sale_price'), 10, 2);
 
             add_filter( 'graphql_woocommerce_price', array( $this, 'graphql_woocommerce_price' ), 10, 5 );
             add_filter( 'graphql_resolve_field', array( $this, 'graphql_resolve_field' ), 99999, 9 );
