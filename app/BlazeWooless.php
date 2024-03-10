91d046a5e4f5ec70cda6a8146c66bb7d03c6764b
zildjianjax <zildjianjaxolis@gmail.com>
2024-03-10 21:06:37 +0800

    Update BlazeWooless.php
    Disabling WooDiscount hooks

--- app/BlazeWooless.php
+++ app/BlazeWooless.php
@@ -90,7 +90,7 @@ class BlazeWooless
 			'\\BlazeWooless\\Extensions\\GraphQL',
 			'\\BlazeWooless\\Extensions\\WoocommerceVariationSwatches',
             '\\BlazeWooless\\Extensions\\WoocommerceProductLabel',
-			'\\BlazeWooless\\Extensions\\WooDiscountRules',
+			// '\\BlazeWooless\\Extensions\\WooDiscountRules',
 		);
 
 		foreach ( $extensions as $extension ) {
