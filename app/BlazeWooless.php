15e8efa81530494d934569e614ff7906d067a1de
Zildjian Jax Olis <zildjianjaxolis@gmail.com>
2024-03-29 00:55:25 -0500

    chore: remove woo discount rules completely

--- app/BlazeWooless.php
+++ app/BlazeWooless.php
@@ -91,7 +91,6 @@ class BlazeWooless
 			'\\BlazeWooless\\Extensions\\GraphQL',
 			'\\BlazeWooless\\Extensions\\WoocommerceVariationSwatches',
             '\\BlazeWooless\\Extensions\\WoocommerceProductLabel',
-			// '\\BlazeWooless\\Extensions\\WooDiscountRules',
 		);
 
 		foreach ( $extensions as $extension ) {
