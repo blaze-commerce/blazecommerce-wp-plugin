edb803cdac8bb093c925b7b71d8056fd49cd75ae
Zildjian Jax Olis <zildjianjaxolis@gmail.com>
2024-04-30 22:15:45 -0500

    fix spacing issue

--- app/Features/Tax.php
+++ app/Features/Tax.php
@@ -3,7 +3,7 @@
 namespace BlazeWooless\Features;
 
 class Tax {
-    private static $instance = null;
+	private static $instance = null;
 
 	public static function get_instance() {
 		if ( self::$instance === null ) {
@@ -20,15 +20,15 @@ class Tax {
 		add_filter( 'blaze_wooless_product_data_for_typesense', array( $this, 'add_price_with_tax_meta_data' ), 10, 3 );
 	}
 
-    public function add_price_with_tax_meta_data( $product_data, $product_id, $product ) {
-        $currency = get_option( 'woocommerce_currency' );
-        if ( !isset( $product_data['metaData'] ) ) {
-            $product_data['metaData'] = array();
-        }
+	public function add_price_with_tax_meta_data( $product_data, $product_id, $product ) {
+		$currency = get_option( 'woocommerce_currency' );
+		if ( !isset( $product_data['metaData'] ) ) {
+			$product_data['metaData'] = array();
+		}
 
-        $product_data['metaData']['priceWithTax'] = array(
-            $currency => floatval( $product->get_price_including_tax() ),
-        );
-        return $product_data;
-    }
+		$product_data['metaData']['priceWithTax'] = array(
+			$currency => floatval( $product->get_price_including_tax() ),
+		);
+		return $product_data;
+	}
 }
