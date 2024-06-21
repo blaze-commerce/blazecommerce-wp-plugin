669a1aa5cb393e8a24291f06c862a409c1db4538
me-jameshwartlopez <me@jameshwartlopez.com>
2024-06-21 13:55:32 +0800

    feat: add component type to taxonomy terms

--- app/Extensions/WoocommerceVariationSwatches.php
+++ app/Extensions/WoocommerceVariationSwatches.php
@@ -2,6 +2,7 @@
 
 namespace BlazeWooless\Extensions;
 
+
 class WoocommerceVariationSwatches {
 	private static $instance = null;
 
@@ -16,13 +17,50 @@ class WoocommerceVariationSwatches {
 	public function __construct() {
 		if ( $this->is_active() ) {
 			add_filter( 'blaze_wooless_product_attribute_for_typesense', array( $this, 'add_swatches_data' ), 10, 2 );
+
+			add_filter( 'blaze_commerce_taxonomy_fields', array( $this, 'add_taxonomy_fields' ) );
+			add_filter( 'blaze_commerce_taxonomy_data', array( $this, 'add_taxonomy_fields_data' ), 10, 2 );
+
 		}
 	}
 
+	public function get_raw_attribute( $taxonomy_slug ) {
+
+		if ( 'pa_' !== substr( $taxonomy_slug, 0, 3 ) ) {
+			return false;
+		}
+		$transient_key      = 'wooless_attribute_' . $taxonomy_slug;
+		$attribute_name     = str_replace( 'pa_', '', wc_sanitize_taxonomy_name( $taxonomy_slug ) );
+		$attribute_taxonomy = get_transient( $transient_key );
+		if ( false === $attribute_taxonomy ) {
+
+			global $wpdb;
+
+			$attribute_taxonomy = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}woocommerce_attribute_taxonomies WHERE attribute_name = %s", esc_sql( $attribute_name ) ) );
+
+			set_transient( $transient_key, $attribute_taxonomy );
+		}
+
+		return $attribute_taxonomy;
+
+	}
 	public function is_active() {
 		return is_plugin_active( 'woo-variation-swatches/woo-variation-swatches.php' );
 	}
 
+	public function add_taxonomy_fields( $fields ) {
+		return array_merge_recursive( $fields, array(
+			array( 'name' => 'componentType', 'type' => 'string', 'facet' => true, 'optional' => true ),
+		) );
+	}
+
+	public function add_taxonomy_fields_data( $document, $term ) {
+		$attribute_taxonomy = $this->get_raw_attribute( $term->taxonomy );
+		if ( ! empty( $attribute_taxonomy->attribute_type ) ) {
+			$document['componentType'] = $attribute_taxonomy->attribute_type;
+		}
+		return $document;
+	}
 
 	public function add_swatches_data( $attribute_to_register, $attribute ) {
 		// Set default attribute type to select
@@ -88,3 +126,5 @@ class WoocommerceVariationSwatches {
 
 	}
 }
+
+
