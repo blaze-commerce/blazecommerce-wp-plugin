1691a27074ce32e31357e55cfd7a7e1a14c2b4e2
Ridwan Arifandi <ridwan@blazecommerce.io>
2024-08-16 19:33:57 +0700

    feat: generate general addons for products

--- app/Extensions/WoocommerceProductAddons.php
+++ app/Extensions/WoocommerceProductAddons.php
@@ -4,6 +4,7 @@ namespace BlazeWooless\Extensions;
 
 class WoocommerceProductAddons {
 	private static $instance = null;
+	private $general_addons = [];
 
 	public static function get_instance() {
 		if ( self::$instance === null ) {
@@ -15,31 +16,18 @@ class WoocommerceProductAddons {
 
 	public function __construct() {
 		if ( is_plugin_active( 'woocommerce-product-addons/woocommerce-product-addons.php' ) ) {
-			add_filter( 'blaze_wooless_additional_site_info', array( $this, 'add_general_product_addons_info' ), 10, 1 );
+			add_action( 'ts_before_product_upsert', array( $this, 'prepare_general_product_addons' ) );
+			add_action( 'blaze_wooless_pre_sync_products', array( $this, 'prepare_general_product_addons' ) );
 			add_filter( 'blaze_wooless_product_data_for_typesense', array( $this, 'sync_product_addons_data' ), 99, 3 );
-
 		}
 	}
 
-	public function add_general_product_addons_info( $site_info ) {
+	public function prepare_general_product_addons() {
 		// we will get all available general product addons
 
 		if ( class_exists( 'WC_Product_Addons_Groups' ) ) {
-
-			$general_addons = \WC_Product_Addons_Groups::get_all_global_groups();
-			$general_addons = blaze_woolese_array_camel_case_keys( $general_addons );
-			foreach ( $general_addons as $index => $addon ) {
-				foreach ( $addon as $key => $value ) {
-					if ( $key === 'restrictToCategories' && count( $value ) > 0 ) {
-						$general_addons[ $index ]['restrictToCategories'] = array_keys( $value );
-					}
-				}
-			}
-
-			$site_info['general_product_addons'] = $general_addons;
+			$this->general_addons = \WC_Product_Addons_Groups::get_all_global_groups();
 		}
-
-		return $site_info;
 	}
 
 	public function sync_product_addons_data( $product_data, $product_id, $product ) {
@@ -49,7 +37,32 @@ class WoocommerceProductAddons {
 			$product_post = get_post( $product_id );
 			$product_addons = blaze_woolese_array_camel_case_keys( \WC_Product_Addons_Product_Group::get_group( $product_post ) );
 
-			$product_data['addons'] = $product_addons;
+			if ( $product_addons['excludeGlobalAddOns'] === false ) {
+				// get product category ids from $product_post
+				$product_categories = wp_get_post_terms( $product_id, 'product_cat', array( 'fields' => 'ids' ) );
+
+				$available_global_addons = [];
+
+				foreach ( $this->general_addons as $addon ) {
+					$restrict_to_categories = $addon['restrict_to_categories'];
+					if ( count( $restrict_to_categories ) === 0 || array_intersect( $product_categories, $restrict_to_categories ) ) {
+						$available_global_addons += $addon['fields'];
+					}
+				}
+
+				if ( ! empty( $available_global_addons ) ) {
+					$product_addons['fields'] += $available_global_addons;
+				}
+
+
+				do_action( 'inspect', [ 
+					"check_available_global_addons", [ 
+						"fields" => $product_addons['fields'],
+					]
+				] );
+			}
+
+			$product_data['addons'] = $product_addons['fields'];
 		}
 
 		return $product_data;
