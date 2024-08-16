8078662db18b473c54808eac255e562f20cfb044
jv-lopez <93750956+jv-lopez@users.noreply.github.com>
2024-08-16 15:10:05 +0800

    refactor: rename tab action hook

--- views/settings.php
+++ views/settings.php
@@ -8,7 +8,7 @@
 		}
 		?>
 		<nav class="nav-tab-wrapper">
-			<?php do_action( 'blaze_wooless_settings_navtab', $active_tab ) ?>
+			<?php do_action( 'blazecommerce/settings/register_tabs', $active_tab ) ?>
 		</nav>
 
 		<?php
