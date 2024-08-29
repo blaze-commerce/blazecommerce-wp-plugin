4ef69c62b610066fef97fa9ac03536f63789dbfe
Ridwan Arifandi <ridwan@blazecommerce.io>
2024-08-29 09:17:08 +0700

    fix: gift card price sync

--- app/Extensions/WoocommerceGiftCards.php
+++ app/Extensions/WoocommerceGiftCards.php
@@ -2,6 +2,8 @@
 
 namespace BlazeWooless\Extensions;
 
+use BlazeWooless\TypesenseClient;
+
 class WoocommerceGiftCards {
 	private static $instance = null;
 
@@ -17,16 +19,18 @@ class WoocommerceGiftCards {
 		if ( is_plugin_active( 'pw-gift-cards/pw-gift-cards.php' ) ) {
 			add_filter( 'blaze_wooless_additional_site_info', array( $this, 'giftcard_email_content' ), 10, 1 );
 			add_filter( 'wooless_product_query_args', array( $this, 'giftcard_product_query_args' ), 10, 1 );
+			add_filter( 'blaze_wooless_product_data_for_typesense', array( $this, 'sync_gift_card_data' ), 99, 3 );
 		}
 	}
 
 	public function giftcard_email_content( $additional_settings ) {
-		if ( $ec_supreme_all_header_logo = get_option( 'ec_supreme_all_header_logo' ) ) {
-			$additional_settings['ec_supreme_all_header_logo'] = $ec_supreme_all_header_logo;
+		if ( $email_header_logo = get_option( 'woocommerce_email_header_image' ) ) {
+			$additional_settings['email_header_logo'] = $email_header_logo;
 		}
 
-		if ( $ec_supreme_all_footer_text = get_option( 'ec_supreme_all_footer_text' ) ) {
-			$additional_settings['ec_supreme_all_footer_text'] = wpautop( $ec_supreme_all_footer_text );
+		if ( $email_footer_text = get_option( 'woocommerce_email_footer_text' ) ) {
+			$email_footer_text = apply_filters( 'woocommerce_email_footer_text', $email_footer_text );
+			$additional_settings['email_footer_text'] = wpautop( $email_footer_text );
 		}
 
 		return $additional_settings;
@@ -42,5 +46,20 @@ class WoocommerceGiftCards {
 		}
 
 		return $args;
+
+	}
+
+	public function sync_gift_card_data( $product_data, $product_id, $product ) {
+
+		if ( is_a( $product, 'WC_Product_PW_Gift_Card' ) ) {
+
+			$price = apply_filters( 'blaze_wooless_calculated_converted_single_price', $product->get_price() );
+
+			$product_data['price'] = $price;
+			$product_data['regularPrice'] = $price;
+			$product_data['salePrice'] = $price;
+		}
+
+		return $product_data;
 	}
 }
