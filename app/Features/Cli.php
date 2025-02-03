224b6890be480ed9f942a240f27e9a6bb8dcdb67
jv-lopez <93750956+jv-lopez@users.noreply.github.com>
2025-02-03 16:53:01 +0800

    feat: cli sync for products

--- app/Features/Cli.php
+++ app/Features/Cli.php
@@ -2,28 +2,66 @@
 
 namespace BlazeWooless\Features;
 
-use BlazeWooless\Woocommerce;
+use BlazeWooless\Collections\Product;
+use WP_CLI;
+use WP_CLI_Command;
 
-class Cli {
-	private static $instance = null;
 
-	public static function get_instance() {
-		if ( self::$instance === null ) {
-			self::$instance = new self();
+class Cli extends WP_CLI_Command {
+
+	/**
+	 * Sync all products.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--all]
+	 * : Sync all products.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp bc-sync product --all
+	 *
+	 * @when after_wp_load
+	 */
+	public function product( $args, $assoc_args ) {
+		if ( isset( $assoc_args['all'] ) ) {
+			WP_CLI::line( "Syncing all products in batches..." );
+
+			$product_collection      = Product::get_instance();
+			$batch_size              = Product::BATCH_SIZE;
+			$page                    = 1;
+			$imported_products_count = 0;
+			$total_imports           = 0;
+
+			do {
+				if ( $page == 1 ) {
+					// recreate the collection to typesense and do some initialization
+					// $product_collection->initialize();
+				}
+
+				$product_ids = $product_collection->get_product_ids( $page, $batch_size );
+				if ( empty( $product_ids ) ) {
+					break; // No more products left to sync
+				}
+
+				$products_batch     = $product_collection->prepare_batch_data( $product_ids );
+				$successful_imports = $product_collection->import_prepared_batch( $products_batch );
+
+				$imported_products_count += count( $successful_imports ); // Increment the count of imported products
+				$total_imports += count( $products_batch ); // Increment the count of imported products
+
+
+				WP_CLI::success( "Completed batch {$page}..." );
+				$page++; // Move to the next batch
+
+			} while ( true );
+
+			WP_CLI::success( "All products have been synced." );
+			WP_CLI::success( "Total batch imported: " . $page );
+			WP_CLI::success( "Total import: " . $total_imports );
+			WP_CLI::success( "Successful import: " . $imported_products_count );
 		}
 
-		return self::$instance;
+		WP_CLI::error( "Nothing was sync" );
 	}
-
-	public function __construct() {
-		if ( defined( 'WP_CLI' ) && WP_CLI ) {
-			add_action( 'cli_init', array( $this, 'init' ) );
-		}
-	}
-
-	public function init() {
-
-	}
-
-
 }
