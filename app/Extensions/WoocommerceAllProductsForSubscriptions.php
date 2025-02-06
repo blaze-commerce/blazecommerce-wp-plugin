ae3f2465e96a657fb1cf66dfc9f41b9b0985e138
Ridwan Arifandi <ridwan@blazecommerce.io>
2025-02-07 05:55:03 +0700

    feat: complete library for WooCommerce All Products for Subscriptions

--- app/Extensions/WoocommerceAllProductsForSubscriptions.php
+++ app/Extensions/WoocommerceAllProductsForSubscriptions.php
@@ -15,14 +15,53 @@ class WoocommerceAllProductsForSubscriptions {
 
 	public function __construct() {
 		if ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'woocommerce-all-products-for-subscriptions/woocommerce-all-products-for-subscriptions.php' ) ) {
-			add_filter( 'wooless_product_query_args', array( $this, 'modify_product_query_args' ), 10 );
+			add_filter( 'blaze_wooless_product_for_typesense_fields', array( $this, 'set_fields' ), 10, 1 );
+			add_filter( 'blaze_wooless_product_data_for_typesense', array( $this, 'sync_product_data' ), 99, 3 );
 		}
 	}
 
-	public function modify_product_query_args( array $args ) {
-		$args['type'] = array_merge( $args['type'], [ 'subscription', 'variable-subscription' ] );
-		return $args;
+	public function set_fields( $fields ) {
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT', 'type' => 'object', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.has_scheme', 'type' => 'boolean', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes', 'type' => 'object[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.period', 'type' => 'string[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.interval', 'type' => 'int64[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.length', 'type' => 'int64[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.trial_period', 'type' => 'string[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.trial_length', 'type' => 'int64[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.pricing_model', 'type' => 'string[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.discount', 'type' => 'int64[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.sync_date', 'type' => 'int64[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.context', 'type' => 'string[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.id', 'type' => 'string[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.key', 'type' => 'string[]', 'optional' => true ];
+		$fields[] = [ 'name' => 'metaData.subscriptionsATT.schemes.is_synced', 'type' => 'boolean[]', 'optional' => true ];
+
+		return $fields;
 	}
 
+	public function sync_product_data( $product_data, $product_id, $product ) {
+		$subscription_schemes = \WCS_ATT_Product_Schemes::get_subscription_schemes( $product );
 
+		$schemes = array_map( function ($scheme) {
+			return $scheme->get_data();
+		}, $subscription_schemes );
+
+		$product_data['metaData']['subscriptionsATT'] = [ 
+			'has_scheme' => \WCS_ATT_Product_Schemes::has_subscription_schemes( $product, 'local' ),
+			'schemes' => $schemes,
+		];
+
+		do_action(
+			"inspect",
+			[ 
+				"sync_product_data_" . rand( 1, 1000 ),
+				[ 
+					"product_data" => $product_data['metaData'],
+				]
+			]
+		);
+
+		return $product_data;
+	}
 }
