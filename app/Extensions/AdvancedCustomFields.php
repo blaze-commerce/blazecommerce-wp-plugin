800c9bf9ab47c4bb62dc410202f24d8f2cf33a4b
jv-lopez <93750956+jv-lopez@users.noreply.github.com>
2025-02-21 12:04:04 +0800

    feat: add acf fields to typesense

--- app/Extensions/AdvancedCustomFields.php
+++ app/Extensions/AdvancedCustomFields.php
@@ -17,6 +17,9 @@ class AdvancedCustomFields {
 		if ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'advanced-custom-fields/acf.php' ) ) {
 			add_filter( 'blaze_wooless_product_for_typesense_fields', array( $this, 'set_fields' ), 10, 1 );
 			add_filter( 'blaze_wooless_product_data_for_typesense', array( $this, 'sync_product_data' ), 99, 3 );
+
+			add_filter( 'blazecommerce/collection/page/typesense_fields', array( $this, 'set_fields' ), 10, 1 );
+			add_filter( 'blazecommerce/collection/page/typesense_data', array( $this, 'set_page_data' ), 10, 2 );
 			add_action( 'admin_init', array( $this, 'test' ) );
 		}
 	}
@@ -29,14 +32,12 @@ class AdvancedCustomFields {
 		return $fields;
 	}
 
-	public function sync_product_data( $product_data, $product_id, $product ) {
-
+	public function get_acf_fields_values( $object_id ) {
 		if ( ! function_exists( 'acf_get_field_groups' ) ) {
-			return $product_data;
+			return null;
 		}
-
 		$field_groups = acf_get_field_groups( [ 
-			'post_id' => $product_id
+			'post_id' => $object_id
 		] );
 
 		$fields = [];
@@ -47,12 +48,28 @@ class AdvancedCustomFields {
 
 		$field_values = [];
 		foreach ( $fields as $field ) {
-			$field_values[ $field['name'] ] = get_field( $field['name'], $product_id );
+			$field_values[ $field['name'] ] = get_field( $field['name'], $object_id );
 		}
 
-		$product_data['metaData']['acf'] = $field_values;
+
+		return $field_values;
+	}
+
+	public function sync_product_data( $product_data, $product_id, $product ) {
+
+		if ( ! function_exists( 'acf_get_field_groups' ) ) {
+			return $product_data;
+		}
+		$product_data['metaData']['acf'] = $this->get_acf_fields_values( $product_id );
 
 		return $product_data;
 	}
 
+	public function set_page_data( $document, $page ) {
+		if ( ! function_exists( 'acf_get_field_groups' ) ) {
+			return $document;
+		}
+		$document['metaData']['acf'] = $this->get_acf_fields_values( $page->ID );
+		return $document;
+	}
 }
