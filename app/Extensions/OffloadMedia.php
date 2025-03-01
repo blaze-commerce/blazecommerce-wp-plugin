47ca0003658e7786ce8288508dec58cf8b28e459
Alan <alan@blaze.online>
2025-03-01 12:02:53 +0800

    refactor: improved code according to wordpress standards

--- app/Extensions/OffloadMedia.php
+++ app/Extensions/OffloadMedia.php
@@ -25,20 +25,20 @@ class OffloadMedia {
 
 	public function page_raw_content_url( $page ) {
 		// Extract the settings from the AS3CF_SETTINGS constant
-		$settings = unserialize(AS3CF_SETTINGS);
+		$settings = unserialize( AS3CF_SETTINGS );
 
 		// Check if bucket and region are set and not empty
-		if (!empty($settings['bucket']) && !empty($settings['region'])) {
-			$new_domain = 'https://' . $settings['bucket'] . '.s3.' . $settings['region'] . '.amazonaws.com';
+		if ( ! empty( $settings['bucket'] ) && ! empty( $settings['region'] ) ) {
+			$new_domain = 'https://' . esc_url( $settings['bucket'] ) . '.s3.' . esc_url( $settings['region'] ) . '.amazonaws.com';
 
 			$pattern = '/<img[^>]+src=[\'"]([^\'"]+)[\'"]/i';
 			
 			// Replace the domain in img src attributes only if it contains /wp-content/uploads
-			$page['rawContent'] = preg_replace_callback($pattern, function($matches) use ($new_domain) {
-				$url = $matches[1];
-				if (strpos($url, '/wp-content/uploads') !== false) {
-					$updated_url = preg_replace('/^https?:\/\/[^\/]+/', $new_domain, $url);
-					return str_replace($url, $updated_url, $matches[0]);
+			$page['rawContent'] = preg_replace_callback( $pattern, function( $matches ) use ( $new_domain ) {
+				$url = esc_url( $matches[1] );
+				if ( strpos( $url, '/wp-content/uploads' ) !== false ) {
+					$updated_url = preg_replace( '/^https?:\/\/[^\/]+/', $new_domain, $url );
+					return str_replace( $url, $updated_url, $matches[0] );
 				}
 				return $matches[0]; // Return unchanged if the condition is not met
 			}, $page['rawContent']);
