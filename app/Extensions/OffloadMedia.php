d5cbeb685be07c5c62ba69a3e50ca1e83ed60b09
Alan <alan@blaze.online>
2025-03-11 22:36:20 +0800

    refactor: clean code and wordpress standards

--- app/Extensions/OffloadMedia.php
+++ app/Extensions/OffloadMedia.php
@@ -6,6 +6,8 @@ class OffloadMedia {
 	private static $instance = null;
 
 	public static function get_instance() {
+		return self::$instance ?? (self::$instance = new self());
+	}
 
 	public function __construct() {
 		if ( $this->is_plugin_active() ) {
@@ -21,30 +23,39 @@ class OffloadMedia {
 		return function_exists( 'is_plugin_active' ) && is_plugin_active( 'amazon-s3-and-cloudfront-pro/amazon-s3-and-cloudfront-pro.php' );
 	}
 
-	public function __construct() {
-		if ( $this->is_plugin_active() ) {
-			add_filter( 'blazecommerce/collection/page/typesense_data', array( $this, 'page_raw_content_url' ), 10, 1 );
+	public function page_raw_content_url( $page ) {
+		if ( ! defined( 'AS3CF_SETTINGS' ) ) {
+			return $page;
 		}
+
+		$settings = unserialize( AS3CF_SETTINGS );
+		if ( empty( $settings['bucket'] ) || empty( $settings['region'] ) ) {
+			return $page;
+		}
+
+		$new_domain = sprintf( 'https://%s.s3.%s.amazonaws.com', $settings['bucket'], $settings['region'] );
+		$page['rawContent'] = $this->replace_urls( $page['rawContent'], $new_domain );
+
+		return $page;
 	}
 
-	public function page_raw_content_url( $page ) {
-		// Check if AS3CF_SETTINGS is defined
-		if (!defined('AS3CF_SETTINGS')) {
-			// Handle the case where AS3CF_SETTINGS is not defined
-			return $page; // or you can throw an exception or log an error
-		}
-
-		// Extract the settings from the AS3CF_SETTINGS constant
-		$settings = unserialize( AS3CF_SETTINGS );
+	private function replace_urls( $content, $new_domain ) {
+		$patterns = [
+			'/<img[^>]+src=[\'"]([^\'"]+)[\'"]/i',
 			'/url":"(https?:\/\/[^"]*\/wp-content\/uploads\/[^"]+)"|url\'\'(https?:\/\/[^"]*\/wp-content\/uploads\/[^"]+)\'/i'
+		];
+
+		foreach ( $patterns as $pattern ) {
+			$content = preg_replace_callback( $pattern, function( $matches ) use ( $new_domain ) {
+				$url = $matches[1] ?? $matches[2];
 				if ( strpos( $url, '/wp-content/uploads' ) !== false ) {
 					$updated_url = preg_replace( '/^https?:\/\/[^\/]+/', $new_domain, $url );
 					return str_replace( $url, $updated_url, $matches[0] );
 				}
-				return $matches[0]; // Return unchanged if the condition is not met
-			}, $page['rawContent']);
+				return $matches[0];
+			}, $content );
 		}
 
-		return $page;
+		return $content;
 	}
 }
